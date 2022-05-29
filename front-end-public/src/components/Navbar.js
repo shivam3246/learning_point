999da9f06a510c6ef44654aaade1dd8d249f1616
CodersDen1 <com.github.adbhut@gmail.com>
2022-05-29 16:55:52 +0530

    Init-nav-bar-commit

---
+++ front-end-public/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css"
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='main-nav'>
+      hey this is Navbar
+    </div>
+  )
+}
+
+export default Navbar
